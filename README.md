a8bd318848163c4a5f8fa4f364d7913805bfb089
Viktor Bengtsson <vibe16@student.bth.se>
2017-12-04 17:41:23 +0100

    Added Docker & Docker Compose

--- README.md
+++ README.md
@@ -9,18 +9,18 @@
 
 You will need:
  * [node.js](https://nodejs.org/en/)
- * Coming Soon - [docker](https://docs.docker.com/engine/installation/)
- * Coming Soon - [docker-compose](https://docs.docker.com/compose/install/)
+ * [docker](https://docs.docker.com/engine/installation/)
+ * [docker-compose](https://docs.docker.com/compose/install/)
 
- ## To start the app
+## To start the app
 ```bash
 - BACKEND
 1. cd backend
 2. yarn - install dependencies
 3. yarn start - start the backend
 
-## Coming Soon
-docker-compose up --build
+- DOCKER
+docker-compose up
 ```
 
 ## Further documentation
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createServer } from 'http';
+import path from 'path';
 
 import contants from './config/constants';
 import middlewares from './config/middlewares';
@@ -12,6 +13,12 @@ app.get('/', (req, res) => {
 });
 
 middlewares(app);
+
+/* 
+This part could be used in production. Just remove the commented lines below and remove the app.get function above. Then git clone https://github.com/Zero2k/bth_ramverk2_frontend and cd bth_ramverk2_frontend and use 'yarn build' to bundle and minimize the frontend. Once it's done, start up the server / backend with 'yarn start' and visit http://localhost:8080
+*/
+// app.use(express.static(path.join(__dirname, '../../frontend/build')));
+// app.use('*', express.static(path.join(__dirname, '../../frontend/build')));
 
 const grahpQLServer = createServer(app);
 
